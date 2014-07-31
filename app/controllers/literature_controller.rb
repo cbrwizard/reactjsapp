11183245c802a5c1c8802ae76c100724f0c9fbf1
cbrwizard <cbrwizard@gmail.com>
2014-07-31 12:59:28 +0400

    Basic structure

---
+++ app/controllers/literature_controller.rb
@@ -0,0 +1,8 @@
+class LiteratureController < ApplicationController
+  # Main page
+  # @note GET /
+  def index
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pushkin = Author.create(name: "Александр Сергеевич Пушкин")
+mayakovskiy = Author.create(name: "Владимир Владимирович Маяковский")
+esenin = Author.create(name: "Есенин, Сергей Александрович")
+
+Book.create([
+  {name: "Капитанская дочка", author: pushkin},
+  {name: "Дубровский", author: pushkin},
+  {name: "Сказка о Царе Салтане", author: pushkin},
+  {name: "Осень", author: esenin},
+  {name: "Пропавший месяц", author: esenin},
+  {name: "Я сам", author: mayakovskiy},
+])
